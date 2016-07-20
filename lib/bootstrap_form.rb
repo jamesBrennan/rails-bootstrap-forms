27f1d95641a4af05dfb67db1e5f4d691f25556a6
James Brennan <james.brennan@educationsuperhighway.org>
2016-07-20 11:49:43 -0700

    Move form helper loading into Engine initialize method

--- lib/bootstrap_form.rb
+++ lib/bootstrap_form.rb
@@ -4,10 +4,11 @@ require 'bootstrap_form/helper'
 module BootstrapForm
   module Rails
     class Engine < ::Rails::Engine
+      initializer "rails-bootstrap-forms.load_view_helpers" do |app|
+        ActiveSupport.on_load(:action_view) do
+          include BootstrapForm::Helper
+        end
+      end
     end
   end
-end
-
-ActiveSupport.on_load(:action_view) do
-  include BootstrapForm::Helper
 end
